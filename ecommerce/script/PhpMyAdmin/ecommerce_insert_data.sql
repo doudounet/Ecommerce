4d9b57a059602b2712b019d5cd93022a2e7e11d8
Ludovic Mantovani <ludovic.mantov@gmail.com>
2014-10-24 16:45:58 +0200

    Ajout insert data dans table membre

--- ecommerce/script/PhpMyAdmin/ecommerce_insert_data.sql
+++ ecommerce/script/PhpMyAdmin/ecommerce_insert_data.sql
@@ -1,3 +1,4 @@
 INSERT INTO `typemembre`(`type`) VALUES ('administrateur');
 INSERT INTO `typemembre`(`type`) VALUES ('utilisateur');
 INSERT INTO `typemembre`(`type`) VALUES ('responsable');
+INSERT INTO `membre`(`login`, `password`, `nom`, `prenom`, `type`) VALUES ('test','test','test','test',1)
