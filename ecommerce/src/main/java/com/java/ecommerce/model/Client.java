5e4cf5dabb41d791aac65303fff7dc7930585f0c
Ludovic Mantovani <ludovic.mantov@gmail.com>
2014-10-28 11:13:55 +0100

    add type to client (modele et mapper)

--- ecommerce/src/main/java/com/java/ecommerce/model/Client.java
+++ ecommerce/src/main/java/com/java/ecommerce/model/Client.java
@@ -8,58 +8,57 @@ public class Client {
 	private String pwdClient;
 	private String loginClient;
 	private String mailClient;
-
+	private String typeClient;
+	@Override
 	public String toString() {
-		return "Client [ prenomClient = " + prenomClient + ", loginClient = " + loginClient + ", idClient = " + idClient + ", prenomClient = " + prenomClient
-				+ ", nomClient = " + nomClient + ", pwdClient = " + pwdClient + ", mailClient = " + mailClient + " ]";
+		return "Client [idClient=" + idClient + ", prenomClient="
+				+ prenomClient + ", nomClient=" + nomClient + ", pwdClient="
+				+ pwdClient + ", loginClient=" + loginClient + ", mailClient="
+				+ mailClient + ", typeClient=" + typeClient + "]";
 	}
-
-	public void setIdClient(Integer idClient) {
-		this.idClient = idClient;
-	}
-
-	public void setLoginClient(String loginClient) {
-		this.loginClient = loginClient;
-	}
-
-	public void setNomClient(String nomClient) {
-		this.nomClient = nomClient;
-	}
-
-	public void setPrenomClient(String prenomClient) {
-		this.prenomClient = prenomClient;
-	}
-
-	public void setPwdClient(String pwdClient) {
-		this.pwdClient = pwdClient;
-	}
-
 	public Integer getIdClient() {
 		return idClient;
 	}
-
-	public String getLoginClient() {
-		return loginClient;
+	public void setIdClient(Integer idClient) {
+		this.idClient = idClient;
 	}
-
-	public String getNomClient() {
-		return nomClient;
-	}
-
 	public String getPrenomClient() {
 		return prenomClient;
 	}
-
+	public void setPrenomClient(String prenomClient) {
+		this.prenomClient = prenomClient;
+	}
+	public String getNomClient() {
+		return nomClient;
+	}
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
 	public String getPwdClient() {
 		return pwdClient;
 	}
-
+	public void setPwdClient(String pwdClient) {
+		this.pwdClient = pwdClient;
+	}
+	public String getLoginClient() {
+		return loginClient;
+	}
+	public void setLoginClient(String loginClient) {
+		this.loginClient = loginClient;
+	}
 	public String getMailClient() {
 		return mailClient;
 	}
-
 	public void setMailClient(String mailClient) {
 		this.mailClient = mailClient;
 	}
+	public String getTypeClient() {
+		return typeClient;
+	}
+	public void setTypeClient(String typeClient) {
+		this.typeClient = typeClient;
+	}
 
+
+	
 }
